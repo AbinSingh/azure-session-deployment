de9bab5facb2cd45f334d72d0610ce6f3cd15f77
Abin Singh R <abin.astro@gmail.com>
2026-08-22 19:38:56 +0530

    "S-AKS A5: Configure FastAPI for Azure Managed Redis"

--- redis_client.py
+++ redis_client.py
@@ -4,10 +4,17 @@ import os
 REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_DB = int(os.getenv("REDIS_DB", "0"))
+REDIS_SSL = os.getenv("REDIS_SSL", "false").lower() == "true"
+
+REDIS_USERNAME = os.getenv("REDIS_USERNAME")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 redis_client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
     db=REDIS_DB,
+    username=REDIS_USERNAME,
+    password=REDIS_PASSWORD,
+    ssl=REDIS_SSL,
     decode_responses=True
 )
